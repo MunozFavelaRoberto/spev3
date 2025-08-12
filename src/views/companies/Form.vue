7b8172a49012168787406bfae200a34fff745f25
Roberto Favela <favelar666@gmail.com>
2025-08-12 14:40:39 -0600

    CRUD companies

--- src/views/companies/Form.vue
+++ src/views/companies/Form.vue
@@ -26,6 +26,7 @@
                       sub
                     />
                   </v-col>
+                  <v-col cols="1" class="text-right" />
                 </v-row>
               </v-card-title>
               <v-card-text>
@@ -42,109 +43,55 @@
                       :rules="rules.textRequired"
                     />
                   </v-col>
-                  <v-col cols="12" md="3">
-                    <v-text-field
-                      label="A. paterno"
-                      v-model="item.surname_p"
-                      type="text"
-                      variant="outlined"
-                      density="compact"
-                      maxlength="25"
-                      counter
-                      :rules="rules.textRequired"
-                    />
-                  </v-col>
-                  <v-col cols="12" md="3">
-                    <v-text-field
-                      label="A. materno*"
-                      v-model="item.surname_m"
-                      type="text"
-                      variant="outlined"
-                      density="compact"
-                      maxlength="25"
-                      counter
-                      :rules="rules.textOptional"
-                    />
-                  </v-col>
                   <v-col cols="12" md="3" class="d-flex align-center" style="gap: 8px">
-                    <!-- File Input -->
                     <v-file-input
-                      label="Fotografía*"
-                      v-model="item.avatar_doc"
+                      label="Logo*"
+                      v-model="item.logo_doc"
                       variant="outlined"
                       density="compact"
                       prepend-icon=""
                       show-size
                       accept=".jpg,.jpeg,.png"
                       :rules="rules.imageOptional"
-                      :disabled="item.avatar_dlt"
+                      :disabled="item.logo_dlt"
                       class="flex-grow-1"
                     >
                       <template v-slot:append>
-                        <div v-if="!isStoreMode && item.avatar && !item.avatar_doc" class="d-flex">
-                          <BtnDwd :value="item.avatar_b64" :disabled="item.avatar_dlt" />
+                        <div v-if="!isStoreMode && item.logo && !item.logo_doc" class="d-flex">
+                          <BtnDwd :value="item.logo_b64" :disabled="item.logo_dlt" />
                         </div>
                       </template>
                     </v-file-input>
-
-                    <!-- Botón de Eliminar (fuera del file-input) -->
                     <v-btn
-                      v-if="!isStoreMode && item.avatar && !item.avatar_doc"
+                      v-if="!isStoreMode && item.logo && !item.logo_doc"
                       icon
                       variant="text"
                       size="small"
-                      :color="item.avatar_dlt ? 'error' : 'default'"
-                      @click="item.avatar_dlt = !item.avatar_dlt"
+                      :color="item.logo_dlt ? 'error' : 'default'"
+                      @click="item.logo_dlt = !item.logo_dlt"
                       class="ml-1"
                       style="margin-top: -20px"
                     >
                       <v-icon size="small">
-                        {{ item.avatar_dlt ? 'mdi-close-circle' : 'mdi-delete' }}
+                        {{ item.logo_dlt ? 'mdi-close-circle' : 'mdi-delete' }}
                       </v-icon>
                       <v-tooltip activator="parent" location="bottom">
-                        {{ item.avatar_dlt ? 'Revertir eliminación' : 'Eliminar' }}
+                        {{ item.logo_dlt ? 'Revertir eliminación' : 'Eliminar' }}
                       </v-tooltip>
                     </v-btn>
                   </v-col>
                 </v-row>
-              </v-card-text>
-            </v-card>
-          </v-col>
-
-          <v-col cols="12">
-            <v-card>
-              <v-card-title>
                 <v-row dense>
                   <v-col cols="12">
-                    <CardTitle text="CUENTA" sub />
-                  </v-col>
-                </v-row>
-              </v-card-title>
-              <v-card-text>
-                <v-row dense>
-                  <v-col cols="12" md="3">
                     <v-text-field
-                      label="E-mail"
-                      v-model="item.email"
+                      label="Descripción*"
+                      v-model="item.description"
                       type="text"
                       variant="outlined"
                       density="compact"
-                      maxlength="65"
+                      maxlength="200"
                       counter
-                      :rules="rules.emailRequired"
-                    />
-                  </v-col>
-                  <v-col cols="12" md="3">
-                    <v-select
-                      label="Rol"
-                      v-model="item.role_id"
-                      variant="outlined"
-                      density="compact"
-                      :items="roles"
-                      item-title="name"
-                      item-value="id"
-                      :rules="rules.required"
-                      :loading="rolesLoading"
+                      :rules="rules.textOptional"
                     />
                   </v-col>
                 </v-row>
@@ -174,35 +121,38 @@
 </template>
 
 <script setup>
-// Importaciones externas
+// Importaciones de librerías externas
 import { ref, inject, onMounted } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
 import axios from 'axios'
 
-// Importaciones internas
+// Importaciones internas del proyecto
 import { useStore } from '@/store'
 import { URL_API } from '@/utils/config'
 import { getHdrs, getErr, getRsp } from '@/utils/http'
-import { getDecodeId, getEncodeId } from '@/utils/coders'
+import { getDecodeId } from '@/utils/coders'
 import { getRules } from '@/utils/validators'
 import { getObj, getFormData } from '@/utils/helpers'
 import { getUserObj } from '@/utils/objects'
+
+// Componentes
 import BtnBack from '@/components/BtnBack.vue'
 import CardTitle from '@/components/CardTitle.vue'
 import BtnDwd from '@/components/BtnDwd.vue'
 
-// Constantes
-const routeName = 'users'
+// Constantes fijas
+const routeName = 'company'
 
-// Estado
+// Estado y referencias
 const alert = inject('alert')
 const confirm = inject('confirm')
 const store = useStore()
 const router = useRouter()
 const route = useRoute()
 
+// Estado reactivo
 const itemId = ref(route.params.id ? getDecodeId(route.params.id) : null)
-const isStoreMode = ref(itemId.value === null)
+const isStoreMode = ref(!itemId.value)
 const isLoading = ref(true)
 const formRef = ref(null)
 const item = ref(null)
@@ -210,31 +160,66 @@ const rules = getRules()
 const roles = ref([])
 const rolesLoading = ref(true)
 
-// Obtener catálogos
-const getCatalogs = async () => {
-  let endpoint = null
-  let response = null
-
-  try {
-    endpoint = `${URL_API}/roles`
-    response = await axios.get(endpoint, getHdrs(store.getAuth?.token))
-    roles.value = getRsp(response).data.items
-  } catch (err) {
-    alert?.show('red-darken-1', getErr(err))
-  } finally {
-    rolesLoading.value = false
-  }
-}
-
-// Obtener registro
+// Obtener datos
 const getItem = async () => {
   if (isStoreMode.value) {
-    item.value = getUserObj()
+    item.value = {
+      id: null,
+      name: null,
+      logo: null,
+      logo_doc: null,
+      logo_dlt: false,
+    }
     isLoading.value = false
   } else {
     try {
-      const endpoint = `${URL_API}/${routeName}/${itemId.value}`
-      const response = await axios.get(endpoint, getHdrs(store.getAuth?.token))
+      //const endpoint = `${URL_API}/${routeName}/${itemId.value}`
+      //const response = await axios.get(endpoint, getHdrs(store.getAuth?.token))
+      const response = {
+        data: {
+         msg: "Registro retornado correctamente",
+  data: {
+    item: {
+      id: 4,
+      active: 1,
+      created_at: "2025-08-06 17:07:14",
+      updated_at: "2025-08-06 20:56:16",
+      created_by_id: 1,
+      updated_by_id: 1,
+      email_verified_at: null,
+      name: "GOOGLE",
+      surname_p: "CASTELLANOS",
+      surname_m: null,
+      email: "ccastellanos@svr.mx",
+      role_id: 2,
+      employment_position: "VENTAS",
+      avatar: "HVUQsNMhDS1od4Y5LVtGIsbxhcouwkc1ct6Dh3zzbKn.png",
+      identification_proof: null,
+      address_proof: null,
+      branch_id: null,
+      uiid: "U-0004",
+      description: "Descripción de la empresa de prueba para el sistema SPE",
+      created_by: {
+        email: "samuel@svr.mx"
+      },
+      updated_by: {
+        email: "samuel@svr.mx"
+      },
+      full_name: "CARLO CASTELLANOS",
+      role: {
+        name: "USUARIO"
+      },
+      branch: null,
+      avatar_b64: {
+        cnt: "iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAA8FBMVEWKbqDnYWb/595KRUWHap6xn7+EZpvXzt7/6+DrYGP/6+KMb6P/6d+Fb6JGQz+HbqHsYGJFQj2We6ZDPz+qkLHth4jpb3JGQUI7ODnmXGLcY25wXXyHbJx1bGnhYmr339qka5LUZHPGZn2sao17ZItPSEzr1c2ViITRvrfz29jMZXmqao17ZItPSEzr1c2ViITRvrfz29jMZXmqao7BZ4CDaZZVTFVeUWJoWHBVT06tnZhiW1qLf3zTusbnztHAp7ybgKi3nbe2aIeVbZr70sudbJbj3ehjVWrDsqvizcWklpF/dHF4YI3TvMLMubMxLzHeiZHOc4XsfX/yoqDwmJf4xb/3vbjZssDtGiLcAAAJ4ElEQVR4nO3ce1faSBgG8AQom4SJoUZLKiKigNJ6KXit1FV0XffS7n7/b7MzCZRbIG+SZ2TYk+cfz+457ea378y8M4NEe1fQ/s8pvNMy4ZonE65/MuH6JxOufzLh+icTrn8y4fonE65/MuH6JxOufzLh+icTAlMa5W3+c6O8iZCrdk9Pm9UgzdPTXe3toNKFXNKsPpy18hsTaZ2df2mevo1SrpDXrnre2tjIz4f/y9Z5VZOPlCnk1Tt/H6YbM9+fVXclI+UJefla75fxRrU8b2oyjbKEpVK1tbR8k8izqkSjJGGpSfb5xrw8oxRhafeBMD7fyChDGLOAI+NZUwpRgrBUTeDzjQ+ymgdeWPoSd4SOifnzKnxhhQtLsafgNNLvkMgHQgtLXxIO0QlkCzohwcLEc3DaeI57IrCw1Ew1RMfEs13YM2GFuxCfCI4IFZbOYcKNc9RcRAoxk3BErIKISOFuCwfkRNA4BQoBjWIqoHEKFO5igfmNU8hj4YTQWegLv0CKCBQSZmElFrEFmYkwYakZXcLKczeOcAOye8MJCb3Q7fW8OELIMIUJKa3CO7aOvBgj9QzxYCghZZDmu/Wc9didKKMbwUVMRJjwIVroPlm5nFXvdT3P5f9Y8V4vj9xlfwDSL2Cj9Cy6hG6b5Xgs67j33O12a72BdblciNi5oYSEdu/1rFwQaxjGakuHKWSpAQkJ09C9GAEncrF8IiI2bihh5IbG7QZjdDoR/fFMIWFIN6xM1se7qIeUkEUIWwoJ57thpVazt4vVjV9e6z7l3L2FzG8Q4zSm1F7d5hR91qjV8q/eq1frtUMKKJac5znhhaJCLeQGym3z5TJXb7f52hlWQCGc74fTq6s6wtMQoTfwXQtwgfBxZp9aqV2qKgxZSvkudAkumIftmRp6vytaw9B2OKzh0sxsatyadbFGQjesAc4VcQpUaVtT/UOdbhHW8CsXkb7ZmegeW9M1VKfjh12zeU+R03BIHA5UlwN5/5gcturs2sKElEHqE9tHHj9OuV5NNE3raUK48aCOcH6UEkvIw6z6ce/yaeA3zalRq9DZYl7oXtAqOKyj9XNTUJ+YhwqdD+eE4ScJEvZoXMT3zfRASULvYsFGlJB6d1xFde5phHB8WHK9o6V7tYgiDn5exyl018aF4rDkupWK63nPg8QF9InHXRfXLHB7GvfJGjw9X9SOHvmRIg1Q9I/nV3HPqNCNcLH424b3aLHgfikdT4RZ7R7fgW/8hjjXQYRX1y8e4SQRy8j7Yvfla1ENYcdg1ivlJBEjXFipWcZVemJ6YfHF4A/UfYULxRW5sZV6gKUXborOYNVek7b4BcLf/XHPrlMXMbWw8NUQD3SJFh57ef6D1TdXLize+MInsJANXi/F0sVuVy+8Gh4J2kigEPqLc/qJCBCKGrK2VwcLPf+nAsKCP0pzuTy2htbxZdBfVz9KC1u+kNXAK81jsIOopwWiugU/1oFXmuPghwLdQit2fGEP2/FzwbQ2bhQQBhORDY6xwmFSt0PIvtQfpm1wDf0Y6QcpQhj0fIZdS4fC1Csp6PTUkTJAORBwtIAIC1tyhKyT3oc6428xGZOwk36ZQQm14m3HMKA8ZrAXDfKrTKBPSAva1g2yjKx+cwuYg/6joX7rq1AEAiFtYvRgsN8RLiIPF5BVNAhQeA0cpsZX2NdcgMIrpBDQ6ofBCYMLG5QQNkiR37e4xQlZR0mhBhS+KCksdGBC4wb3jUGgsPgCW2qACw20hjeoYcrS385MPBZwHsKWGsDtzDjQ75CidjWA25lxkELYrgbwidM4UCFqIiKnIVSIOusDDxYa+tvqmIkI3HZrYKH/cXD6MMjtxShQIWbzDe0V8PdiIITILZuGFkL6hYF9Xw5WiBimwAuM4JnA76cBrKbQdQYuTN3oGUPuZ0TQNSxcpyIyA/GLXtNPBH8TVioiQ+65g0h4m1knMZHltuBAGcLCi5GsZxj1TQkvOJTxzr3iTZKPophxjfkoZiZS3ptY3Io/UlkOvsYEkfPuy4J2FW+kMqOzKQco7f2l4hNFupHl8GvoKNLeQVvQbnLUoWpcyyqgJvU9wsXNK9IHw0ZdQo8YR+q7oIu3lLPGTUHq67Ylv8+7GHnYYOid9mwkC4e/uZgJZSYTpv3rM2EmBDxCJkz512dCIXTk/fc1yULH2f8jWvjnvS3TKFFoa4c75e3o711+KDf2HXlGaUK7f7dn6h8pQl03dw75/xA5kSN07P6Bbuo6VciNe3eSjDKEjnPfKAtfDCE3mgd9GYMVL+TLS6OsDxNDKIyfJCw6aKFjf94xdT2RUBgbcCNW6DiHk77YQt+4jzUihY52uFfW9VRCHnDzwAltTbQHPb1QNI/POCNK6DghPi78K4nQN6LGKkbI51+YTxD/jiBa2x9D/6C5AxqrCKFYX8qhjxlNtL6HA3XYfEwvnOkP88Tvi4kstxgo6ojoHWmFjn2/1CeI24uu963cX8uAvrGfdi+XUmj3Py0cn2Oi/iPs9Z7M+vExAsiJ5YOU+9VUQls7MSMKOCrjt1kjs74tWGNmjfqdlmaophA6dmiDCCd+3P4xfiebeD/bj+3oAo6Me2laR3Jh9AScMerf//nGmHiL97d/vn8g+3xjiumYVOhoB9ETcA7JWR8+BD/jxTRPkg7VhEJ7UYeXFr4DSFbGREJHa7yxzzceJNoAJBHah/oKgGLFuU9QxvhCRyO0QFnGk/hVjC20+7GWUDSxEXvBiSu0P6+O5xNjj9SYQudkZSP0p/EwHjGe0F7dFBynfBeLGEtor76CIuXDOHMxjtDZX+EaM5W+LOHOqmXDmJ9ijNMYQudQlRLyItLHaRyhKiUUGzh6EelC51CJZSZImT4T6UJbnRKK7Ru5iHRhX6ES6voeXmifqLPO8Jj71LWGLHT2Vo2aCn2toQrV6fbD7BGBZKF9oJiQPEzJo1SllVSEvJoShc69YiXU9R1sDZ075YQmselThY1Vg+ZiEs9Q1HmoXAnJBwya0PmsnlDXkULleoUIcfdNFKrWK0SIE5E4DxUsIXXjRhIqt2ULsgMUqtcN/ZAmIkloN1ZtCQ1ta0qbh2qdnEYx71BCBTelQRqUiUgSKjoN9R0CkCS0PykqNCnXpqR5qGK/FyH1fIqwr2gJaT2fIFRz2+2ngamhYveIU4kGkmrYWLVjYcr30UWkCFftWBxKz48WOmpd50+FstQQhAp9bDgXwvEiWqjk+X4UMxJIEara70XM6KWGsNKsWrEshKUmUqjswcIP4UoxWqjqwSJI9FITKVT2YDFM5E1GdA1VXmgoNxlhWFyW/qoNy2PeLX16kXfar78sy7/q7mhEzD+WPr3Ir/8BAFUhZ9Lgh2sAAAAASUVORK5CYII=",
+        ext: "png"
+      },
+      avatar_doc: null,
+      avatar_dlt: false
+    }
+  }
+        },
+      };
       item.value = getRsp(response).data.item
     } catch (err) {
       alert?.show('red-darken-1', getErr(err))
@@ -262,16 +247,16 @@ const handleAction = async () => {
 
   try {
     const endpoint = `${URL_API}/${routeName}${!isStoreMode.value ? `/${payload.id}` : ''}`
-    const response = getRsp(
-      await axios.post(endpoint, getFormData(payload), getHdrs(store.getAuth?.token, true))
-    )
+    const response = getRsp(await axios.post(endpoint, getFormData(payload), getHdrs(store.getAuth?.token, true)))
+    
+
 
     alert?.show('success', response.msg)
 
     router.push({
       name: `${routeName}/show`,
       params: {
-        id: getEncodeId(isStoreMode.value ? response.data.item.id : itemId.value),
+        id: btoa(isStoreMode.value ? response.data.item.id : itemId.value),
       },
     })
   } catch (err) {
@@ -281,9 +266,8 @@ const handleAction = async () => {
   }
 }
 
-// Cargar datos al montar
+// Inicialización
 onMounted(() => {
-  getCatalogs()
   getItem()
 })
 </script>
--- src/views/companies/Show.vue
+++ src/views/companies/Show.vue
@@ -9,27 +9,26 @@
         <v-col v-if="item" cols="2" class="text-right">
           <div class="d-flex justify-end" style="gap: 10px">
             <v-btn
-              v-if="item.active"
               icon
               variant="flat"
               size="x-small"
-              color="info"
-              @click="showCredential"
+              color="primary"
+              :to="{ name: `` }"
             >
-              <v-icon>mdi-card-account-details</v-icon>
-              <v-tooltip activator="parent" location="bottom">Ver credencial</v-tooltip>
+              <v-icon>mdi mdi-store</v-icon>
+              <v-tooltip activator="parent" location="bottom">Sucursales</v-tooltip>
             </v-btn>
-            <v-btn
-              v-if="item.active"
-              icon
-              variant="flat"
-              size="x-small"
-              color="warning"
-              :to="{ name: `${routeName}/update`, params: { id: getEncodeId(itemId) } }"
-            >
-              <v-icon>mdi-pencil</v-icon>
-              <v-tooltip activator="parent" location="bottom">Editar</v-tooltip>
-            </v-btn>
+          <v-btn
+            v-if="item.active"
+            icon
+            variant="flat"
+            size="x-small"
+            color="warning"
+            :to="{ name: `${routeName}/update`, params: { id: getEncodeId(itemId) } }"
+          >
+            <v-icon>mdi-pencil</v-icon>
+            <v-tooltip activator="parent" location="left">Editar</v-tooltip>
+          </v-btn>
           </div>
         </v-col>
       </v-row>
@@ -38,14 +37,20 @@
     <v-card-text v-if="item">
       <v-row>
         <v-col v-if="!item.active" cols="12">
-          <v-alert color="red-darken-3" variant="outlined" density="compact">
+          <v-alert type="error" density="compact" class="rounded">
             <v-row dense>
-              <v-col class="grow pt-2">
-                <v-icon start class="mr-1" size="x-small">mdi-alert</v-icon>
-                Registro inactivo
-              </v-col>
+              <v-col class="grow pt-2">El registro se encuentra inactivo</v-col>
               <v-col v-if="store.getAuth?.user?.role_id === 1" class="shrink text-right">
-                <v-btn size="x-small" color="success" @click.prevent="restoreItem"> Activar </v-btn>
+                <v-btn
+                  icon
+                  variant="flat"
+                  size="x-small"
+                  color="success"
+                  @click.prevent="restoreItem"
+                >
+                  <v-icon>mdi-delete-restore</v-icon>
+                  <v-tooltip activator="parent" location="left">Reactivar</v-tooltip>
+                </v-btn>
               </v-col>
             </v-row>
           </v-alert>
@@ -56,7 +61,7 @@
             <v-card-title>
               <v-row dense>
                 <v-col cols="11">
-                  <CardTitle :text="`GENERAL | ${item.uiid}`" sub />
+                  <CardTitle :text="'GENERAL | ' + item.uiid" sub />
                 </v-col>
                 <v-col cols="1" class="text-right">
                   <v-btn
@@ -72,53 +77,31 @@
                 </v-col>
               </v-row>
             </v-card-title>
+
             <v-card-text>
               <v-row dense>
                 <v-col cols="12" md="3">
                   <VisVal label="Nombre" :value="item.name" />
                 </v-col>
                 <v-col cols="12" md="3">
-                  <VisVal label="A. paterno" :value="item.surname_p" />
-                </v-col>
-                <v-col cols="12" md="3">
-                  <VisVal label="A. materno" :value="item.surname_m" />
-                </v-col>
-                <v-col cols="12" md="3">
-                  <VisDoc label="Fotografía" :value="item.avatar_b64" img />
+                  <VisDoc label="Logo" :value="item.logo_b64" img />
                 </v-col>
               </v-row>
-            </v-card-text>
-          </v-card>
-        </v-col>
-
-        <v-col cols="12">
-          <v-card>
-            <v-card-title>
-              <v-row dense>
+              <v-row>
                 <v-col cols="12">
-                  <CardTitle text="CUENTA" sub />
-                </v-col>
-              </v-row>
-            </v-card-title>
-            <v-card-text>
-              <v-row dense>
-                <v-col cols="12" md="3">
-                  <VisVal label="E-mail" :value="item.email" />
-                </v-col>
-                <v-col cols="12" md="3">
-                  <VisVal label="Rol" :value="item.role?.name" />
+                  <VisVal label="Descripción" :value="item.description" />
                 </v-col>
               </v-row>
             </v-card-text>
           </v-card>
         </v-col>
 
-        <v-col v-if="item.active" cols="12">
+        <v-col v-if="item.active && store.getAuth?.user?.role_id === 1" cols="12">
           <v-btn
             icon
             variant="flat"
             size="x-small"
-            color="red-darken-3"
+            color="red-darken-1"
             @click.prevent="deleteItem"
           >
             <v-icon>mdi-minus-thick</v-icon>
@@ -129,53 +112,81 @@
     </v-card-text>
 
     <DlgReg v-model="regDialog" :item="item" />
-
-    <DlgCredential v-model="credentialDialog" :loading="credentialLoading" :img="credentialImage" />
   </v-card>
 </template>
 
 <script setup>
-// Importaciones externas
+// Importaciones de librerías externas
 import { ref, inject, onMounted } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
 import axios from 'axios'
 
-// Importaciones internas
+// Importaciones internas del proyecto
 import { useStore } from '@/store'
 import { URL_API } from '@/utils/config'
 import { getHdrs, getErr, getRsp } from '@/utils/http'
 import { getDecodeId, getEncodeId } from '@/utils/coders'
+
+// Componentes
 import BtnBack from '@/components/BtnBack.vue'
 import CardTitle from '@/components/CardTitle.vue'
 import DlgReg from '@/components/DlgReg.vue'
 import VisVal from '@/components/VisVal.vue'
 import VisDoc from '@/components/VisDoc.vue'
-import DlgCredential from '@/components/DlgCredential.vue'
 
-// Constantes
-const routeName = 'users'
+// Constantes fijas
+const routeName = 'company'
 
-// Estado
+// Estado y referencias
 const alert = inject('alert')
 const confirm = inject('confirm')
 const store = useStore()
 const router = useRouter()
 const route = useRoute()
 
+// Estado reactivo
 const itemId = ref(getDecodeId(route.params.id))
 const isLoading = ref(true)
 const item = ref(null)
 const regDialog = ref(false)
-const credentialDialog = ref(false)
-const credentialLoading = ref(false)
-const credentialImage = ref(null)
 
 // Obtener registro
 const getItem = async () => {
   isLoading.value = true
   try {
-    const endpoint = `${URL_API}/${routeName}/${itemId.value}`
-    const response = await axios.get(endpoint, getHdrs(store.getAuth?.token))
+    //const endpoint = `${URL_API}/${routeName}/${itemId.value}`
+    //const response = await axios.get(endpoint, getHdrs(store.getAuth?.token))
+    const response = {
+      data: {
+        msg: "Registro retornado correctamente",
+  data: {
+    item: {
+      id: 1,
+      active: 1,
+      created_at: "2025-07-31 17:30:23",
+      updated_at: "2025-07-31 17:30:23",
+      created_by_id: 1,
+      updated_by_id: 1,
+      name: "GOOGLE",
+      logo: "cDdfLTsehXzcHn7jbydu3mBPUY36Q7nekBpMTZ3BCdB.png",
+      uiid: "E-0001",
+      description: "Descripción de la empresa de prueba para el sistema SPE",
+      created_by: {
+        email: "samuel@svr.mx"
+      },
+      updated_by: {
+        email: "samuel@svr.mx"
+      },
+      logo_b64: {
+        cnt: "iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAA8FBMVEWKbqDnYWb/595KRUWHap6xn7+EZpvXzt7/6+DrYGP/6+KMb6P/6d+Fb6JGQz+HbqHsYGJFQj2We6ZDPz+qkLHth4jpb3JGQUI7ODnmXGLcY25wXXyHbJx1bGnhYmr339qka5LUZHPGZn2sao17ZItPSEzr1c2ViITRvrfz29jMZXmqao7BZ4CDaZZVTFVeUWJoWHBVT06tnZhiW1qLf3zTusbnztHAp7ybgKi3nbe2aIeVbZr70sudbJbj3ehjVWrDsqvizcWklpF/dHF4YI3TvMLMubMxLzHeiZHOc4XsfX/yoqDwmJf4xb/3vbjZssDtGiLcAAAJ4ElEQVR4nO3ce1faSBgG8AQom4SJoUZLKiKigNJ6KXit1FV0XffS7n7/b7MzCZRbIG+SZ2TYk+cfz+457ea378y8M4NEe1fQ/s8pvNMy4ZonE65/MuH6JxOufzLh+icTrn8y4fonE65/MuH6JxOufzLh+icTrn8y4fonE65/MuH6JxOufzLh+icTAlMa5W3+c6O8iZCrdk9Pm9UgzdPTXe3toNKFXNKsPpy18hsTaZ2df2mevo1SrpDXrnre2tjIz4f/y9Z5VZOPlCnk1Tt/H6YbM9+fVXclI+UJefla75fxRrU8b2oyjbKEpVK1tbR8k8izqkSjJGGpSfb5xrw8oxRhafeBMD7fyChDGLOAI+NZUwpRgrBUTeDzjQ8ymgdeWPoSd4SOifnzKnxhhQtLsafgNNLvkMgHQgtLXxIO0QlkCzohwcLEc3DaeI57IrCw1Ew1RMfEs13YM2GFuxCfCI4IFZbOYcKNc9RcRAoxk3BErIKISOFuCwfkRNA4BQoBjWIqoHEKFO5igfmNU8hj4YTQWegLv0CKCBQSZmElFrEFmYkwYakZXcLKczeOcAOye8MJCb3Q7fW8OELIMIUJKa3CO7aOvBgj9QzxYCghZZDmu/Wc9didKKMbwUVMRJjwIVroPlm5nFXvdT3P5f9Y8V4vj9xlfwDSL2Cj9Cy6hG6b5Xgs67j33O12a72BdblciNi5oYSEdu/1rFwQaxjGakuHKWSpAQkJ09C9GAEncrF8IiI2bihh5IbG7QZjdDoR/fFMIWFIN6xM1se7qIeUkEUIWwoJ57thpVbzhs2v4rlPuZAK5ljEKM0rJJwfpJWaNejV8q/eq1frtUMKKJac5znhhaJCLeQGym3z5TJXb7f52hlWQCGc74fTq6s6wtMQoTfwXQtwgfBxZp9aqV2qKgxZSvkudAkumIftmRp6vytaw9B2OKzh0sxsatyadbFGQjesAc4VcQpUaVtT/UOdbhHW8CsXkb7ZmegeW9M1VKfjh12zeU+R03BIHA5UlwN5/5gcturs2sKElEHqE9tHHj9OuV5NNE3raUK48aCOcH6UEkvIw6z6ce/yaeA3zalRq9DZYl7oXtAqOKyj9XNTUJ+YhwqdD+eE4ScJEvZoXMT3zfRASULvYsFGlJB6d1xFde5phHB8WHK9o6V7tYgiDn5exyl018aF4rDkupWK63nPg8QF9InHXRfXLHB7GvfJGjw9X9SOHvmRIg1Q9I/nV3HPqNCNcLH424b3aLHgfikdT4RZ7R7fgW/8hjjXQYRX1y8e4SQRy8j7Yvfla1ENYcdg1ivlJBEjXFipWcZVemJ6YfHF4A/UfYULxRW5sZV6gKUXborOYNVek7b4BcLf/XHPrlMXMbWw8NUQD3SJFh57ef6D1TdXLize+MInsJANXi/F0sVuVy+8Gh4J2kigEPqLc/qJCBCKGrK2VwcLPf+nAsKCP0pzuTy2htbxZdBfVz9KC1u+kNXAK81jsIOopwWiugU/1oFXmuPghwLdQit2fGEP2/FzwbQ2bhQQBhORDY6xwmFSt0PIvtQfpm1wDf0Y6QcpQhj0fIZdS4fC1Csp6PTUkTJAORBwtIAIC1tyhKyT3oc6428xGZOwk36ZQQm14m3HMKA8ZrAXDfKrTKBPSAva1g2yjKx+cwuYg/6joX7rq1AEAiFtYvRgsN8RLiIPF5BVNAhQeA0cpsZX2NdcgMIrpBDQ6ofBCYMLG5QQNkiR37e4xQlZR0mhBhS+KCksdGBC4wb3jUGgsPgCW2qACw20hjeoYcrS385MPBZwHsKWGsDtzDjQ75CidjWA25lxkELYrgbwidM4UCFqIiKnIVSIOusDDxYa+tvqmIkI3HZrYKH/cXD6MMjtxShQIWbzDe0V8PdiIITILZuGFkL6hYF9Xw5WiBimwAuM4JnA76cBrKbQdQYuTN30GUPuZ0TQNSxcpyIyA/GLXtNPBH8TVioiQ+65g0h4m1knMZHltuBAGcLCi5GsZxj1TQkvOJTxzr3iTZKPophxjfkoZiZS3ptY3Io/UlkOvsYEkfPuy4J2FW+kMqOzKQco7f2l4hNFupHl8GvoKNLeQVvQbnLUoWpcyyqgJvU9wsXNK9IHw0ZdQo8YR+q7oIu3lLPGTUHq67Ylv8+7GHnYYOid9mwkC4e/uZgJZSYTpv3rM2EmBDxCJkz512dCIXTk/fc1yULH2f8jWvjnvS3TKFFoa4c75e3o711+KDf2HXlGaUK7f7dn6h8pQl03dw75/xA5kSN07P6Bbuo6VciNe3eSjDKEjnPfKAtfDCE3mgd9GYMVL+TLS6OsDxNDKIyfJCw6aKFjf94xdT2RUBgbcCNW6DiHk77YQt+4jzUihY52uFfW9VRCHnDzwAltTbQHPb1QNI/POCNK6DghPi78K4nQN6LGKkbI51+YTxD/jiBa2x9D/6C5AxqrCKFYX8qhjxlNtL6HA3XYfEwvnOkP88Tvi4kstxgo6ojoHWmFjn2/1CeI24uu963cX8uAvrGfdi+XUmj3Py0cn2Oi/iPs9Z7M+vExAsiJ5YOU+9VUQls7MSMKOCrjt1kjs74tWGNmjfqdlmaophA6dmiDCCd+3P4xfiebeD/bj+3oAo6Me2laR3Jh9AScMerf//nGmHiL97d/vn8g+3xjiumYVOhoB9ETcA7JWR8+BD/jxTRPkg7VhEJ7UYeXFr4DSFbGREJHa7yxzzceJNoAJBHah/oKgGLFuU9QxvhCRyO0QFnGk/hVjC20+7GWUDSxEXvBiSu0P6+O5xNjj9SYQudkZSP0p/EwHjGe0F7dFBynfBeLGEtor76CIuXDOHMxjtDZX+EaM5W+LOHOqmXDmJ9ijNMYQudQlRLyItLHaRyhKiUUGzh6EelC51CJZSZImT4T6UJbnRKK7Ru5iHRhX6ES6voeXmifqLPO8Jj71LWGLHT2Vo2aCn2toQrV6fbD7BGBZKF9oJiQPEzJo1SllVSEvJoShc69YiXU9R1sDZ075YQmselThY1Vg+ZiEs9Q1HmoXAnJBwya0PmsnlDXkULleoUIcfdNFKrWK0SIE5E4DxUsIXXjRhIqt2ULsgMUqtcN/ZAmIkloN1ZtCQ1ta0qbh2qdnEYx71BCBTelQRqUiUgSKjoN9R0CkCS0PykqNCnXpqR5qGK/FyH1fIqwr2gJaT2fIFRz2+2ngamhYveIU4kGkmrYWLVjYcr30UWkCFftWBxKz48WOmpd50+FstQQhAp9bDgXwvEiWqjk+X4UMxJIEara70XM6KWGsNKsWrEshKUmUqjswcIP4UoxWqjqwSJI9FITKVT2YDFM5E1GdA1VXmgoNxlCWFyW/qoNy2PeLX16kXfar78sy7/q7mhEzD+WPr3Ir/8BAFUhZ9Lgh2sAAAAASUVORK5CYII=",
+        ext: "png"
+      },
+      logo_doc: null,
+      logo_dlt: false
+    }
+  }
+      },
+    };
     item.value = getRsp(response).data.item
   } catch (err) {
     alert?.show('red-darken-1', getErr(err))
@@ -193,7 +204,7 @@ const deleteItem = async () => {
   try {
     const endpoint = `${URL_API}/${routeName}/${itemId.value}`
     const response = getRsp(await axios.delete(endpoint, getHdrs(store.getAuth?.token)))
-    alert?.show('red-darken-3', response.msg)
+    alert?.show('red-darken-1', response.msg)
     router.push({ name: routeName })
   } catch (err) {
     alert?.show('red-darken-1', getErr(err))
@@ -209,7 +220,7 @@ const restoreItem = async () => {
 
   isLoading.value = true
   try {
-    const endpoint = `${URL_API}/${routeName}/restore`
+    const endpoint = `${URL_API}/system/${routeName}/restore`
     const response = getRsp(
       await axios.post(endpoint, { id: itemId.value }, getHdrs(store.getAuth?.token))
     )
@@ -222,31 +233,8 @@ const restoreItem = async () => {
   }
 }
 
-// Mostrar credencial
-const showCredential = async () => {
-  credentialImage.value = null
-  credentialDialog.value = true
-  credentialLoading.value = true
-
-  try {
-    const endpoint = `${URL_API}/${routeName}/dni`
-    const response = await axios.post(
-      endpoint,
-      { user_id: itemId.value },
-      getHdrs(store.getAuth?.token)
-    )
-    const data = getRsp(response).data
-
-    if (!data.img64) throw new Error('No se recibió una imagen válida')
-
-    credentialImage.value = data.img64
-  } catch (err) {
-    alert?.show('red-darken-1', getErr(err))
-  } finally {
-    credentialLoading.value = false
-  }
-}
-
-// Cargar datos al montar
-onMounted(getItem)
+// Inicializar
+onMounted(() => {
+  getItem()
+})
 </script>
